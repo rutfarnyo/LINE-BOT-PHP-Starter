dc4b18479d724337de51d2c299103b807226d52f
rutfarnyo <nirut0518@gmail.com>
2017-08-08 17:25:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,9 +13,11 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'GoldPrice') {
 			// Get text sent
-			$text = "\n".$events2['text']."\nทองแท่งขาย : ".$events2['sale1']."\nทองแท่งซื้อ : ".$events2['buy1']."\nทองรูปประพรรณขาย : ".$events2['sale2']."\nทองรูปประพรรณซื้อ : ".$events2['buy2'];
+			//$text = "\n".$events2['text']."\nทองแท่งขาย : ".$events2['sale1']."\nทองแท่งซื้อ : ".$events2['buy1']."\nทองรูปประพรรณขาย : ".$events2['sale2']."\nทองรูปประพรรณซื้อ : ".$events2['buy2'];
 			// Get replyToken
+			
 			$replyToken = $event['replyToken'];
+			$text = $replyToken;
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
