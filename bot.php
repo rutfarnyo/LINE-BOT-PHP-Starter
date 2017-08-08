cb40a7935f5878964464e720df10210cb4ada2e8
rutfarnyo <nirut0518@gmail.com>
2017-08-08 14:53:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text.' auto reply from revones report bot'
+				'text' => $text
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
